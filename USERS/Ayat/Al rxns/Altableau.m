3709b4f9f39c7ebe2de40e970c5cc6fdc52361a9
Scott Smith <ssmith@wlu.ca>
2024-07-15 10:51:35 +0000

    July 15 backup

--- USERS/Ayat/Al rxns/Altableau.m
+++ USERS/Ayat/Al rxns/Altableau.m
@@ -1,4 +1,4 @@
-function [AlOH3,MASSERR]=Altableau(pH,pe,T,flag1,flag2,flag3,flag4,flag5)
+function [Al,AlOH3s,Al13,AlOH4,MASSERR]=Altableau(pH,pe,T,flag1,flag2,flag3,flag4,flag5)
 
 % input tableau.  change this part % ----------------------------------------------
 
@@ -9,12 +9,13 @@ Tableau=[...
 0       0        1           0                            0        {'Al'}
 -1      0        0           -14                          0        {'OH'}
 -1      0        1           -4.9571                      0        {'AlOH'}
-%-2      0        1           -10.5945                     0        {'AlOH2'}
-%-2      0        2           -7.6902                      0        {'Al2OH2'}
-%-4      0        3           -13.8803                     0        {'Al3OH4'}
-%-32      0       13           -98.73                       0        {'Al13'}
+-2      0        1           -10.5945                     0        {'AlOH2'}
+-4      0        1           -22.7                        0        {'AlOH4'}
+-2      0        2           -7.6902                      0        {'Al2OH2'}
+-4      0        3           -13.8803                     0        {'Al3OH4'}
+-32     0        13          -98.73                       0        {'Al13'}
 %solid
--3      0        1          7.7560                       1        {'AlOH3'}
+-3      0        1           -7.7560                      1        {'AlOH3s'}
 ];
 
 % end of tableau.  ------------------ % ----------------------------------------------
