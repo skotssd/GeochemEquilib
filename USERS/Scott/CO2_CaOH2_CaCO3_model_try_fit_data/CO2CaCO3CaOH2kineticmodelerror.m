67fa19b91b02a1c234d2c4409890d16e3120c71c
Scott Smith <ssmith@wlu.ca>
2026-04-23 17:12:32 -0400

    modelling cement data

---
+++ USERS/Scott/CO2_CaOH2_CaCO3_model_try_fit_data/CO2CaCO3CaOH2kineticmodelerror.m
@@ -0,0 +1,206 @@
+function [detZtZ]=CO2CaCO3CaOH2kineticmodelerror(p,XP,time,data)
+
+kP=10^p(1); kC=10^p(2); kCO2=10^p(3); KspC=10^p(4);
+sampletime=data(:,1); samplepH=data(:,2); sampleDIC=(data(:,3)*1e-3); conduct=data(:,4); sampleCa=(data(:,5)*1e-3); 
+sampleSi=(data(:,6)*1e-3);
+
+%stll need housekeeping?
+% set path to equilibrium solver
+txt=pwd; n=length(txt); rootpath=txt;
+n=strfind(rootpath,"USERS"); rootpath=txt(1:n-2);
+PHREEQCpath=[rootpath,'/runPHREEQC'];
+addpath(PHREEQCpath);
+JEQUILIBpath=[rootpath,'/JEQUILIB'];
+addpath(JEQUILIBpath)
+% turn off warnings
+warning off
+
+% fixed variables
+
+XC=1e-6; % need seed crystal of calcite.  batstone paper used 1e-6
+%equilib constants
+KspP=0.00000660693;  % from https://www.aqion.de/site/16 0.00000660693
+%KspC=3.31131121e-9;  % Ksp for calcite
+%KspC=4.7e-9;  % Ksp for calcite
+%KspC=10^-7.33; % Ksp for monhydrocalcite
+logPCO2=-3.3;   % PCO2. measured at 468.4 ppm average in the room
+database=['llnl.dat'];
+
+%write phreeqc file
+solutionboxtext=[...
+{'SOLUTION 1\n'}
+{'       pe      13.75\n'}
+{'       pH      7\n'}
+{'       temp    25\n'}
+{'-units mol/kgw\n'}
+   ];
+[nolinesSOLN,length]=size(solutionboxtext);
+rateboxtext=[...
+{'RATES\n'}
+{'Portlandite\n'}
+{'-start\n'}
+{'1   REM   PARM(1) = rate constant. PARM(2) Ksp for portlandite \n'}
+{'2 Ca=Act("Ca+2")\n'}
+{'3 OH=Act("OH-")\n'}
+{'4 IAP=Ca*OH^2\n'}
+{'5 Ksp=PARM(2)\n'}
+{'6 si_pp=log(IAP/Ksp)\n'}
+{'20  IF (M <= 0  and si_pp < 0) THEN GOTO 200\n'}
+{'120 rate = PARM(1) *M* (1 - 10^(si_pp))\n'}
+{'140 moles = rate*TIME\n'}
+{'200 SAVE moles\n'}
+{'   -end\n'}
+{'Calcite\n'}
+{'-start\n'}
+{'1   REM   PARM(1) = rate constant. Parm(2)=Ksp calcite\n'}
+{'2 Ca=Act("Ca+2")\n'}
+{'3 CO3=Act("CO3-2")\n'}
+{'4 IAP=Ca*CO3\n'}
+{'5 Ksp=PARM(2)\n'}
+{'6 si_cc=log(IAP/Ksp)\n'}
+{'20  IF (M <= 0  and si_cc < 0) THEN GOTO 200\n'}
+{'120 rate = PARM(1) *M* (1 - 10^(si_cc))\n'}
+{'140 moles = rate*TIME\n'}
+{'200 SAVE moles\n'}
+{'   -end\n'}
+{'CO2_dissolution\n'}
+{'-start\n'}
+{'20 k = parm(2)\n'}
+{'30 eq_HCO3 = 10^PARM(1)*10^LK_PHASE("CO2(g)")/ACT("H+")\n'}
+{'40 act_HCO3 = ACT("HCO3-")\n'}
+{'50 moles = k * (1-(act_HCO3/eq_HCO3)) * TIME\n'}
+{'60 SAVE moles\n'}
+{'   -end\n'}
+];
+[nolinesRATE,length]=size(rateboxtext);
+XPstr=num2str(XP);
+KspPstr=num2str(KspP);
+XCstr=num2str(XC);
+kPstr=num2str(kP);
+kCstr=num2str(kC);
+KspCstr=num2str(KspC);
+logPCO2str=num2str(logPCO2);
+kCO2str=num2str(kCO2);
+
+m0Pline=['  -m0    ',XPstr,'\n'];
+mPline=['  -m    ',XPstr,'\n'];
+parmsPline=['  -parms ',kPstr,' ',KspPstr,'\n'];
+m0Cline=['  -m0    ',XCstr,'\n'];
+mCline=['  -m    ',XCstr,'\n'];
+parmsCline=['  -parms ',kCstr,' ',KspCstr,'\n'];
+parmsCO2line=['  -parms ',logPCO2str,' ',kCO2str,'\n'];
+timestr=mat2str([0 time]); timestr = regexprep(timestr,'\[','');  timestr = regexprep(timestr,'\]','');
+timeline=[' -steps ',timestr,' s\n'];
+
+kineticsboxtext=[...
+{'KINETICS 1\n'}
+{'Portlandite\n'}
+{'  -tol   1e-13\n'}
+{m0Pline}
+{mPline}
+{parmsPline}
+{'Calcite\n'}
+{'  -tol   1e-13\n'}
+{m0Cline}
+{mCline}
+{parmsCline}
+{'CO2_dissolution\n'}
+{'  -tol   1e-13\n'}
+{'-formula CO2 1\n'}
+{parmsCO2line}
+{timeline}
+{'  -cvode\n'}
+{'-runge_kutta      6\n'}
+];
+[nolinesKIN,length]=size(kineticsboxtext);
+selectboxtext=[...
+{' SELECTED_OUTPUT\n'}
+{' -file portout.txt\n'}
+{' -selected_out true\n'}
+{' -user_punch true\n'}
+{' -high_precision true\n'}
+{' -reset false\n'}
+{' -simulation false\n'}
+{' -state false\n'}
+{' -distance false\n'}
+{' -time false\n'}
+{' -step false\n'}
+{' -ph false\n'}
+{' -pe false\n'}
+{' -reaction false\n'}
+{' -temperature false\n'}
+{' -alkalinity false\n'}
+{' -ionic_strength false\n'}
+{' -water false\n'}
+{' -charge_balance false\n'}
+{' -percent_error false\n'}
+];
+[nolinesSEL,length]=size(selectboxtext);
+punchboxtext=[...
+{'USER_PUNCH\n'}
+{'        -headings seconds  Ca  pH Calcite  Portlandite DIC\n'}
+{'  10 PUNCH SIM_TIME, TOT("Ca"), -LA("H+"), KIN("Calcite"), KIN("Portlandite"), TOT("C") \n'}
+];
+[nolinesPUNCH,length]=size(punchboxtext);
+fileID=fopen('porttest.txt','w');
+
+fprintf(fileID,'TITLE TEST PORTLANDITE DISSOLUTION\n');
+
+for i=1:nolinesSOLN
+    line=cell2mat(solutionboxtext(i,:));
+    fprintf(fileID,line)
+end
+fprintf(fileID,'\n');
+for i=1:nolinesRATE
+    line=cell2mat(rateboxtext(i,:));
+    fprintf(fileID,line)
+end
+fprintf(fileID,'\n');
+for i=1:nolinesKIN
+    line=cell2mat(kineticsboxtext(i,:));
+    fprintf(fileID,line)
+end
+fprintf(fileID,'\n');
+for i=1:nolinesSEL
+    line=cell2mat(selectboxtext(i,:));
+    fprintf(fileID,line)
+end
+fprintf(fileID,'\n');
+for i=1:nolinesPUNCH
+    line=cell2mat(punchboxtext(i,:));
+    fprintf(fileID,line)
+end
+fprintf(fileID,'\n');
+fprintf(fileID,'END');
+fclose(fileID);
+%str=['system("phreeqc porttest.txt out.txt ', database,'");'];
+str=['system(" ',PHREEQCpath, '/phreeqc porttest.txt out.txt ', database,'");'];
+%eval(str); % output to the screen
+evalc(str); % so no screen output
+fid = fopen('portout.txt','rt');
+hdr = strtrim(regexp(fgetl(fid),'\t','split'));
+hdr=hdr(1:6)';
+mat = cell2mat(textscan(fid,repmat('%f',1,numel(hdr))));
+[nsize,msize]=size(mat);
+time=mat(2:nsize,1); Caphreeqc=mat(2:nsize,2); pHphreeqc=mat(2:nsize,3);
+calcitephreeqc=mat(2:nsize,4); portlanditephreeqc=mat(2:nsize,5);
+DIC=mat(2:nsize,6);
+
+
+Ca=Caphreeqc;
+pH=pHphreeqc;
+
+yi = interp1 (time, pH, sampletime);
+residual1=samplepH-yi;
+
+yi = interp1 (time, Ca, sampletime);
+residual2=sampleCa*1e3-yi*1e3;
+
+yi = interp1 (time, DIC, sampletime);
+residual3=sampleDIC*1e3-yi*1e3;
+
+Z=[residual1 residual2 residual3];
+%Z=[residual1 residual3];
+detZtZ=det(Z'*Z);
+
+end
